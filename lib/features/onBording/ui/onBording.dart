bcb57d1aae746a378e54f16a4db57a655dc6ddc4
hanyy1167 <hanyy1167@gmail.com>
2025-01-01 16:04:19 +0200

    no message

--- lib/features/onBording/ui/onBording.dart
+++ lib/features/onBording/ui/onBording.dart
@@ -8,8 +8,7 @@ class OnbordingScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
-      body: OnboardingPageView()
+      body: OnboardingPageView(),
     );
   }
 }
